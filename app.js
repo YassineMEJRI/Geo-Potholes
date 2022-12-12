a1c7fe78665d35ee09855b9efd9473b09d0c3994
Yassine Mejri <yassinemejri5@gmail.com>
2022-12-12 01:22:28 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const fs = require('fs');
+const path = require('path');
+
+const imgModel = require('./image.model')
+
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'uploads')
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.fieldname + '-' + Date.now())
+	}
+});
+
+var upload = multer({ storage: storage });
+
+
+mongoose.connect("mongodb://localhost:27017/potholes",
+    { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+        console.log('connected')
+    });
+
+
+
+// App
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+ 
+// Set EJS as templating engine
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.get('/map', (req, res) => {
+    res.render('map')
+});
+
+app.get('/images', (req, res) => {
+	imgModel.find({}, (err, items) => {
+		if (err) {
+			console.log(err);
+			res.status(500).send('An error occurred', err);
+		}
+		else {
+			res.render('imagesPage', { items: items });
+		}
+	});
+});
+
+app.post('/upload', upload.single('image'), (req, res, next) => {
+
+	var obj = {
+		name: req.body.name,
+		desc: req.body.desc,
+		img: {
+			data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+			contentType: 'image/png'
+		}
+	}
+	imgModel.create(obj, (err, item) => {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			res.redirect('/images');
+		}
+	});
+});
+
+
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
+
