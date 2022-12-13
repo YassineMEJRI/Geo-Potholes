dbc06263e818dead123cd6ef785f4b40b41eb60e
Yassine Mejri <yassinemejri5@gmail.com>
2022-12-13 10:34:22 +0100

    imageSchema now contains date_added, date_fixed, and state

--- image.model.js
+++ image.model.js
@@ -3,6 +3,9 @@ var mongoose = require('mongoose');
 var imageSchema = new mongoose.Schema({
 	name: String,
 	desc: String,
+	date_added: Date,
+	state: String,
+	date_fixed: Date,
 	img:
 	{
 		data: Buffer,
